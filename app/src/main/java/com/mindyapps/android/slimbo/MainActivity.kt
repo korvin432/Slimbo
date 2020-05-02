97ed9e0ff35c12d10c41aa54cacc239642b0b620
Roman Kr <roma_as@list.ru>
2020-05-02 21:37:35 +0300

    bottom navigation state save, sleep fragment layout preparations

--- app/src/main/java/com/mindyapps/android/slimbo/MainActivity.kt
+++ app/src/main/java/com/mindyapps/android/slimbo/MainActivity.kt
@@ -2,40 +2,63 @@ package com.mindyapps.android.slimbo
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
 import android.view.WindowManager
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_sleep,
-                R.id.navigation_history,
-                R.id.navigation_relax,
-                R.id.navigation_more
-            )
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+
+        val navGraphIds =
+            listOf(R.navigation.sleep, R.navigation.history, R.navigation.relax, R.navigation.more)
+
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        controller.observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController)
+        })
+        currentNavController = controller
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
             WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
         )
         supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
--- app/src/main/java/com/mindyapps/android/slimbo/ui/sleep/SleepFragment.kt
+++ app/src/main/java/com/mindyapps/android/slimbo/ui/sleep/SleepFragment.kt
@@ -1,31 +1,32 @@
 package com.mindyapps.android.slimbo.ui.sleep
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.mindyapps.android.slimbo.R
+
 
 class SleepFragment : Fragment() {
 
     private lateinit var sleepViewModel: SleepViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         sleepViewModel =
-                ViewModelProviders.of(this).get(SleepViewModel::class.java)
+            ViewModelProviders.of(this).get(SleepViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_sleep, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        sleepViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val loginActivityBackground: Drawable =
+            root.findViewById<RelativeLayout>(R.id.sleep_layout).background
+        loginActivityBackground.alpha = 35
         return root
     }
 }
