0a099994eedf2ef641b4a08e7c26eb393317b26e
Roman Kr <roma_as@list.ru>
2020-05-30 23:36:12 +0300

    Hiding rating bar tip

--- app/src/main/java/com/mindyapps/slimbo/ui/recording/RecordingFragment.kt
+++ app/src/main/java/com/mindyapps/slimbo/ui/recording/RecordingFragment.kt
@@ -11,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RatingBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -80,6 +81,7 @@ class RecordingFragment : Fragment() {
         } else {
             sleepRatingBar.rating = recording.rating!!.toFloat()
             sleepRatingBar.setIsIndicator(true)
+            root.findViewById<TextView>(R.id.how_do_you_feel_textView).visibility = View.GONE
             sleepRatingBar.isClickable = false
         }
 
