dce2d86ad08b3efc3f33b3203d2ffb860117afd8
Roman Kr <roma_as@list.ru>
2020-06-01 23:28:58 +0300

    created mediaplayer and seekbar

--- app/src/main/java/com/mindyapps/slimbo/ui/relax/RelaxFragment.kt
+++ app/src/main/java/com/mindyapps/slimbo/ui/relax/RelaxFragment.kt
@@ -3,9 +3,12 @@ package com.mindyapps.slimbo.ui.relax
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -21,11 +24,14 @@ import com.mindyapps.slimbo.R
 import com.mindyapps.slimbo.data.model.Music
 import com.mindyapps.slimbo.data.repository.SlimboRepositoryImpl
 import com.mindyapps.slimbo.ui.adapters.RelaxMusicAdapter
-import com.mindyapps.slimbo.ui.adapters.SelectMusicAdapter
+import kotlinx.android.synthetic.main.fragment_relax.*
 import kotlinx.coroutines.launch
 import java.io.File
+import java.util.*
+import kotlin.math.ln
 
-class RelaxFragment : Fragment() {
+
+class RelaxFragment : Fragment(), View.OnClickListener {
 
     private var repository = SlimboRepositoryImpl()
     private lateinit var viewModel: RelaxViewModel
@@ -34,6 +40,18 @@ class RelaxFragment : Fragment() {
     private lateinit var storage: FirebaseStorage
     private lateinit var observerMusic: Observer<List<Music>>
     var player: MediaPlayer? = null
+
+    var birdsPlayer: MediaPlayer? = null
+    var cricketPlayer: MediaPlayer? = null
+    var natureSoundsPlayer: MediaPlayer? = null
+    var pinkNoisePlayer: MediaPlayer? = null
+    var rainInCarPlayer: MediaPlayer? = null
+    var rainOnTheRoofPlayer: MediaPlayer? = null
+    var streetNoisePlayer: MediaPlayer? = null
+    var thunderPlayer: MediaPlayer? = null
+    var waterStreamPlayer: MediaPlayer? = null
+
+    var playersList: List<MediaPlayer> = LinkedList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,6 +78,8 @@ class RelaxFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setSubscriber()
+
+        birds_card.setOnClickListener(this)
     }
 
     private fun setSubscriber() {
@@ -84,7 +104,7 @@ class RelaxFragment : Fragment() {
 
     private fun stopPlaying() {
         if (player != null && player!!.isPlaying) player!!.stop()
-        if (musicAdapter.mediaPlayer != null && musicAdapter.mediaPlayer!!.isPlaying){
+        if (musicAdapter.mediaPlayer != null && musicAdapter.mediaPlayer!!.isPlaying) {
             musicAdapter.mediaPlayer!!.stop()
         }
     }
@@ -139,6 +159,42 @@ class RelaxFragment : Fragment() {
                         player!!.start()
                     }
                 }
+            }
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.birds_card -> {
+                if (birdsPlayer == null) {
+                    val resID = requireContext().resources.getIdentifier(
+                        "birds", "raw", requireContext().packageName
+                    )
+                    birdsPlayer = MediaPlayer.create(context, resID)
+                    birdsPlayer!!.isLooping = true
+                    birdsPlayer!!.start()
+
+                    birds_check.visibility = View.VISIBLE
+                    birds_seekbar.visibility = View.VISIBLE
+
+                    birds_seekbar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+                        override fun onProgressChanged(seekBar: SeekBar, progress: Int, b: Boolean) {
+                            val volume = (1 - ln((100 - progress).toDouble()) / ln(
+                                    100.toDouble())).toFloat()
+                            birdsPlayer!!.setVolume(volume, volume)
+                        }
+                        override fun onStartTrackingTouch(seekBar: SeekBar) {}
+                        override fun onStopTrackingTouch(seekBar: SeekBar) {}
+                    })
+
+                } else {
+                    birds_check.visibility = View.GONE
+                    birds_seekbar.visibility = View.GONE
+                    birds_seekbar.progress = 100
+                    birdsPlayer!!.stop()
+                    birdsPlayer = null
+                }
+
             }
         }
     }
